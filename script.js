9195c2e814238cfab242dec2933c523bd0faf7a1
Ryan Lacy <rlacy15@gmail.com>
2022-08-28 10:39:30 -0500

    Created functions to build grid and change cover on mouse hover

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// Creates a 16x16 grid
+// Function that creates a 16x16 grid
+function createGrid() {
     const GRID_SIZE = 16;
-    var container = document.querySelector(".grid-container");
-    container.style.setProperty("--grid-rows", GRID_SIZE);
-    container.style.setProperty("--grid-cols", GRID_SIZE);
-    for (var row = 0; row < (GRID_SIZE*GRID_SIZE); row++) {
+    let container = document.querySelector(".grid-container");
+
+    // TODO - better understand how these properties work
+    container.style.gridTemplateColumns = `repeat(${GRID_SIZE}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${GRID_SIZE}, 1fr)`;
+
+    // Create the grid, give them a class, and add event listeners
+    for (var row = 0; row < GRID_SIZE; row++) {
         for (var col = 0; col < GRID_SIZE; col++) {
             var cell = document.createElement("div");
             cell.classList.add("square");
             container.appendChild(cell);
+            cell.addEventListener("mouseover", changeColor);
         }
     }
+}
+
+function changeColor(event) {
+    event.target.style.backgroundColor = "black";
+}
+
+createGrid();
